019f930dbc374e2cefd14be263d77aa5f318f054
Link <nt85188@cathaybk.com.tw>
2022-12-04 13:16:37 +0800

    Initial Commit

---
+++ MovieDBSwiftUI/MovieDBSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBSwiftUIApp.swift
+//  MovieDBSwiftUI
+//
+//  Created by Link on 2022/12/4.
+//
+
+import SwiftUI
+
+@main
+struct MovieDBSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
